b06f2e30dfc3d92a64a52b34a040e90713f4eb16
Mario <s303988@studenti.polito.it>
2023-03-29 00:00:15 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include "declaredFun.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    //getDba("test.db"); // function implemented by Mario
+
+    // getDba("dba.ini"); // function implemented by Mario
     AeroDB db0; // create db object
     db0 = readData("dba_2000.ini"); // Open database and print it to the screen
-    int a = 10;
+
     return 0;
 }
