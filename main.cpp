0f9381a78ddb23a2454a8b6639287398509b0666
stlbw <matheush.padilha@gmail.com>
2023-03-28 17:53:33 +0200

    updated main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "declaredFun.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    getDba("test.db"); // function implemented by Mario
+    //getDba("test.db"); // function implemented by Mario
     AeroDB db0; // create db object
     db0 = readData("dba_2000.ini"); // Open database and print it to the screen
 
