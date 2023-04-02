d6bf45b9c5c7712b061e134aca52bf602021ec2c
stlbw <matheush.padilha@gmail.com>
2023-04-02 13:40:18 +0200

    minor change to dataCheckSwitch declaration

--- main.cpp
+++ main.cpp
@@ -26,11 +26,11 @@ int main() {
     cout << "\t2 - Print full database to screen" << endl;
     cout << "\t3 - Save database to file" << endl;
     cout << ">> ";
-    char dataCheckSwitch; // used to store the user's choice
+    char dataCheckSwitch = '1'; // used to store the user's choice
     bool flagCaseFound = false;
     bool printToFile;
     while(!flagCaseFound) {
-        cin >> dataCheckSwitch; // get user input
+        //cin >> dataCheckSwitch; // get user input
         switch (dataCheckSwitch) {
             case '1':
                 cout << "Printing partial version to screen..." << endl;
