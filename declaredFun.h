b0ad653c2f9f2b5b481b467434d38907f08ac82b
stlbw <matheush.padilha@gmail.com>
2023-04-12 16:25:22 +0200

    update

--- declaredFun.h
+++ declaredFun.h
@@ -3,7 +3,7 @@
 
 #include "preprocessing/readAeroDb.cpp"
 #include "preprocessing/readBatEnPropDb.cpp"
-#include "interpolation.cpp"
+#include "src/interpolation.cpp"
 #include "preprocessing/printAll.cpp"
 
 #endif
