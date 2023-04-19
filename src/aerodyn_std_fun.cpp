0207421d344332698886136dc18f1b55aee95655
stlbw <matheush.padilha@gmail.com>
2023-04-19 19:30:37 +0200

    feat: added aerodyn_std_fun.cpp containing function to compute density

---
+++ src/aerodyn_std_fun.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Matheus Padilha on 19/04/23.
+//
+#include <cmath>
+/**
+ * Computes the air density at a certain altitude using a simplified mathematical model
+ * @param h
+ * @return
+ */
+double computeDensity(double h) {
+    double rho_SL = 1.225;
+    double grad = 0.0065;
+    double m = 4.2561;
+    double T_0 = 288.15;
+    double ans = ((T_0 - grad * h) / T_0);
+    double rho = rho_SL * pow(ans, m);
+
+    return rho;
+}
