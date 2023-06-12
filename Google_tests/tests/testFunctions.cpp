7121ac253b5cfa40d1febb635293d8b5a33ae31b
stlbw <matheush.padilha@gmail.com>
2023-06-12 20:09:56 +0200

    added integrateMotion test function

--- Google_tests/tests/testFunctions.cpp
+++ Google_tests/tests/testFunctions.cpp
@@ -62,3 +62,50 @@ TEST(TrimTest, CanComputeDeltaTrim) {
     double tol = 0.05;
     ASSERT_NEAR(trimAngles(db1, db2, V, h).deltae_trim, -2.16, tol);
 }
+
+TEST(IntegrateMotion, CanMaintainTrim) {
+    double V = 15;
+    double h = 100;
+    double vecRef[12] = {14.9873, 0, 0.617672, 0, 0, 0, 0, 0.0411898, 0, 100, 0, 0};
+    double vecTrim[12] = {14.9873, 0, 0.617672, 0, 0, 0, 0, 0.0411898, 0, 100, 0, 0};
+    double vecComTrim[4] = {0, -0.0376, 0, 0.260227};
+    AeroDB db1, db2, db3, db4, DB1, DB2;
+    db1 = readData("dba.ini", true);
+    db2 = readData("dba_100.ini", true);
+    db3 = readData("dba_1000.ini", true);
+    db4 = readData("dba_2000.ini", true);
+    BatteryDB bat0;
+    EngineDB en0; // create dba objects from struct type
+    PropDB prop0; // create dba objects from struct type
+    // read not aerodynamic database
+    bat0 = readBat("battery.ini"); // Open database, read it and save data to struct of type Not_Aerodb
+    en0 = readEn("engine.ini"); // Open database, read it and save data to struct of type Not_Aerodb
+    prop0 = readProp("propeller.ini"); // Open database, read it and save data to struct of type PropDB
+
+    std::ofstream outputSim("testIntegrateMotion.txt");
+    std::ofstream loggerRemainders("testIntegrateMotion_logRemainders.txt");
+    std::ofstream loggerAcceleration("testIntegrateMotion_logAcceleration.txt");
+    double stateMinusOne[12] = {0}; // i-1
+    double rpm = getRpm(vecComTrim[3], en0.laps_min, en0.laps_max);//d_th * RPM_MAX
+    for (int j = 0; j < 12; j++) {
+        stateMinusOne[j] = vecTrim[j]; // during trim the (i-1)th state is the same as the trim. We assume the
+        // simulation starts at the last trim state (i=0) and before that we assume infinite trim states take place
+    }
+    getAerodynamicDbWithAltitude(h, DB1, DB2, db1, db2, db3, db4);
+
+    for (int i = 1; i < 10000; i++) {
+        h = vecTrim[9];
+        double* newStatesPointer = integrateEquationsOfMotion(DB1, DB2, en0, prop0, rpm, vecTrim, vecComTrim, stateMinusOne, 0.01, loggerRemainders, loggerAcceleration);
+        double newStates[12] = {0};
+        for (int j = 0; j < 12; j++) {
+            newStates[j] = newStatesPointer[j]; // save the recently calculated state vector in newStates
+            stateMinusOne[j] = vecTrim[j]; // save the OLD initial condition as the (i-1)th step
+            vecTrim[j] = newStates[j]; // update the initial condition vector with the new states for the next loop iteration
+        } // assign values to variable
+        delete[] newStatesPointer; // delete pointer to avoid memory leak
+        EXPECT_NEAR(vecTrim[0], vecRef[0], 1);
+        EXPECT_NEAR(vecTrim[9], vecRef[9], 1);
+
+    }
+
+}
